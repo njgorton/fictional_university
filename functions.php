9c7229a605bfd7061edd1be41a99d798ed852fee
Nathaniel Gorton <gorton.nj@gmail.com>
2018-07-17 10:43:35 -0500

    bulid programs section and post type

--- functions.php
+++ functions.php
@@ -16,6 +16,12 @@
     add_action('after_setup_theme', 'university_features');
     
     function university_adjust_queries ($query) {
+        IF (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+            $query->set('orderby', 'title');
+            $query->set('order', 'ASC');
+            $query->set('posts_per_page', -1);
+        }
+
         if (!is_admin() AND is_post_type_archive('event') AND $query->is_main_query()) {
             $today = date('Ymd');
             $query->set('meta_key', 'event_date');
