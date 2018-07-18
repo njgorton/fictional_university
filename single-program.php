714f48def52eca973c2b4e2a83b201ce9bc90d94
Nathaniel Gorton <gorton.nj@gmail.com>
2018-07-18 15:06:30 -0500

    build professors post type

--- single-program.php
+++ single-program.php
@@ -23,6 +23,39 @@
             <div class="generic-content"><?php the_content(); ?></div>
 
             <?php
+                $relatedProfessors = new WP_Query(array(
+                    'posts_per_page' => -1,
+                    'post_type' => 'professor',
+                    'orderby' => 'title',
+                    'order' => 'ASC',
+                    'meta_query' => array(
+                        array(
+                            'key' => 'related_programs',
+                            'compare' => 'LIKE',
+                            'value' => '"' . get_the_ID() . '"'
+                        )
+                    )
+                ));
+    
+                if ($relatedProfessors->have_posts()) {
+                    echo '<hr class="section-break">';
+                    echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' Professors</h2>';
+    
+                    echo '<ul class="professor-cards">';
+                    while ($relatedProfessors->have_posts()) {
+                        $relatedProfessors->the_post(); ?>
+                        <li class="professor-card__list-item">
+                            <a class="professor-card" href="<?php the_permalink(); ?>">
+                                <img src="<?php the_post_thumbnail_url('professorLandscape'); ?>" class="professor-card__img">
+                                <span class="professor-card__name"><?php the_title(); ?></span>
+                            </a>
+                        </li>
+                    <?php } 
+                    echo '</ul>';
+                } 
+
+                wp_reset_postdata();
+
                 $today = date('Ymd');
                 $homepageEvents = new WP_Query(array(
                 'posts_per_page' => 2,
