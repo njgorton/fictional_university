a4cf4546231652cc7eb69b1d95a58b91f77eb06d
Nathaniel Gorton <gorton.nj@gmail.com>
2018-07-20 14:45:23 -0500

    build campus section and post type

--- functions.php
+++ functions.php
@@ -29,6 +29,7 @@
     <?php }
 
     function university_files() {
+        wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyAM5aZw7HJYgZc15wabh5Ft0hvnqKo1rCM', NULL, microtime(), true);
         wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true);
         wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
         wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -48,7 +49,11 @@
     add_action('after_setup_theme', 'university_features');
     
     function university_adjust_queries ($query) {
-        IF (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+        if (!is_admin() AND is_post_type_archive('campus') AND $query->is_main_query()) {
+            $query->set('posts_per_page', -1);
+        }
+
+        if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
             $query->set('orderby', 'title');
             $query->set('order', 'ASC');
             $query->set('posts_per_page', -1);
@@ -71,4 +76,11 @@
     }
 
     add_action('pre_get_posts', 'university_adjust_queries');
+
+    function universityMapKey($api) {
+        $api['key'] = 'AIzaSyAM5aZw7HJYgZc15wabh5Ft0hvnqKo1rCM';
+        return $api;
+    }
+
+    add_filter('acf/fields/google_map/api', 'universityMapKey');
 ?>
