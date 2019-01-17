cd76d2f712003a6518357bd52860274c8d6155e3
njgorton <gorton.nj@gmail.com>
2019-01-17 12:26:02 -0600

    setup traditional WP search.

---
+++ searchform.php
@@ -0,0 +1,8 @@
+<form class="search-form" method="GET" action="<?php echo esc_url(site_url('/')); ?>">
+    <label class="headline headline--medium" for="s">Perform a New Search:</label>
+    
+    <div class="search-form-row">
+        <input class="s" type="search" id="s" name="s" placeholder="What are you looking for?">
+        <input class="search-submit" type="submit" value="Search">
+    </div>
+</form>
