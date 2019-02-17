6add2f8238b4ff6c9e98a32d26a0e5338aa7f8d7
njgorton <gorton.nj@gmail.com>
2019-02-17 11:22:25 -0600

    finish myNotes feature with full CRUD.

--- functions.php
+++ functions.php
@@ -4,7 +4,15 @@
 
     function university_custom_rest() {
         register_rest_field('post', 'authorName', array(
-            'get_callback' => function() {return get_the_author();}
+            'get_callback' => function() {
+                return get_the_author();
+            }
+        ));
+
+        register_rest_field('note', 'userNoteCount', array(
+            'get_callback' => function() {
+                return count_user_posts(get_current_user_id(), 'note');
+            }
         ));
     }
 
@@ -140,6 +148,27 @@
 
     function ourLoginTitle() {
         return get_bloginfo('name');
+    }
+
+
+    // Force Note Posts To Be PRIVATE.
+    add_filter('wp_insert_post_data', 'makeNotePrivate', 10, 2);
+
+    function makeNotePrivate($data, $postarr) {
+        if($data['post_type'] == 'note') {
+            if(count_user_posts(get_current_user_id(), 'note') > 4 AND !$postarr['ID']) {
+                die("You have reached your note limit!");
+            }
+
+            $data['post_content'] = sanitize_textarea_field($data['post_content']); 
+            $data['post_title'] = sanitize_text_field($data['post_title']);
+        }
+
+        if($data['post_type'] == 'note' AND $data['post_status'] != 'trash') {
+            $data['post_status'] = "private";
+        }
+
+        return $data;
     }
 
 ?>
--- page-my-notes.php
+++ page-my-notes.php
@@ -12,7 +12,16 @@
         pageBanner();
         ?>
 
-  <div class="container container--narrow page-section">
+    <div class="container container--narrow page-section">
+    
+        <div class="create-note">
+            <h2 class="headline headline--medium">Create New Note</h2>
+            <input class="new-note-title" placeholder="Title">
+            <textarea class="new-note-body" placeholder="Your note here..."></textarea>
+            <span class="submit-note">Create Note</span>
+            <span class="note-limit-message">Note limit reached: delete an existing note to make room for a new one.</span>
+        </div>
+
         <ul class="min-list link-list" id="my-notes">
             <?php
                 $userNotes = New WP_Query(array(
@@ -25,17 +34,17 @@
                     $userNotes->the_post(); ?>
 
                     <li data-id="<?php the_ID(); ?>">
-                        <input readonly class="note-title-field" value="<?php echo esc_attr(get_the_title()); ?>">
+                        <input readonly class="note-title-field" value="<?php echo str_replace('Private: ', '', esc_attr(get_the_title())); ?>">
                         <span class="edit-note"><i class="fa fa-pencil" aria-hidden="true"></i> Edit</span>
                         <span class="delete-note"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</span>
-                        <textarea readonly class="note-body-field"><?php echo esc_attr(get_the_content()); ?></textarea>
+                        <textarea readonly class="note-body-field"><?php echo esc_textarea(get_the_content()); ?></textarea>
                         <span class="update-note btn btn--blue btn--small"><i class="fa fa-arrow-right" aria-hidden="true"></i> Save</span>
                     </li>
 
                 <?php }
             ?>
         </ul>
-  </div>
+    </div>
     
     <?php
     }
